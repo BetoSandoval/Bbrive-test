f5761a49280da536a12a4de637f069149510e243
BetoSandoval <alberto.sa1391@gmail.com>
2022-01-13 02:22:28 -0600

    Agregando Link al header y al boton add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,23 @@
-import { Box, Button, Flex, Heading, Spacer, Container } from "@chakra-ui/react";
+import { Box, Button, Flex, Container, Heading } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
-    <Container mt={50} maxW='container.xl'>
-      <Flex justify='right'>
+    <Container mt={50} maxW="container.xl">
+
+      <Link to="/">
+      <Box p="2" justify="left">
+        <Heading size="md">Lista de Empleados App</Heading>
+      </Box>
+      </Link>
+
+      <Flex justify="right">
         <Box>
-          <Button colorScheme="teal" size="md">
-            + Agregar nuevo empleado
-          </Button>
+          <Link to="/add">
+            <Button colorScheme="teal" size="md">
+              + Agregar nuevo empleado
+            </Button>
+          </Link>
         </Box>
       </Flex>
     </Container>
