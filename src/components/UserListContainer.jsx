390cc5cd9f5a0560dafe4b0fbb6d2237dc9be9fd
BetoSandoval <alberto.sa1391@gmail.com>
2022-01-13 02:24:21 -0600

    Consuminedo el contextp en UserListContainer

--- src/components/UserListContainer.jsx
+++ src/components/UserListContainer.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, {useContext} from "react";
+import { GlobalContext } from "../context/GlobalContext";
 import { UserCard } from "./UserCard";
 import { Container } from "@chakra-ui/react";
 
 export const UserListContainer = () => {
+
+  const {users} = useContext(GlobalContext);
+
   return (
     <>
       <Container maxW="xl" mt={50} centerContent>
-          <UserCard/>
+        { users.map( user => {
+          return (
+          <UserCard
+            key={user.id}
+            img={user.img}
+            name={user.name}
+            phone={user.phone}
+            email={user.email}
+          />
+          )
+        } ) }
       </Container>
     </>
   );
