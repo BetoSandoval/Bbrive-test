62f0ee080aa7f240cf091bd8542ad13572ac62a1
BetoSandoval <alberto.sa1391@gmail.com>
2022-01-13 02:21:53 -0600

    Agregando rutas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import { Button } from "@chakra-ui/react";
 import { UserListContainer } from "./components/UserListContainer";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { UserForm } from "./components/UserForm";
 import { Header } from "./components/Header";
 
