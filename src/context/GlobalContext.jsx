656dd6315d9434583935e49688e841890454e3b4
BetoSandoval <alberto.sa1391@gmail.com>
2022-01-13 02:23:32 -0600

    creando global context y esatdo incial

---
+++ src/context/GlobalContext.jsx
@@ -0,0 +1,34 @@
+import { createContext } from "react";
+
+export const GlobalContext = createContext({
+    users: [
+        {
+            id: 1,
+            img: 'https://bit.ly/dan-abramov',
+            name: 'Dan Abramov',
+            phone: 5652829,
+            email: 'danabramov@gmail.com'
+        },
+        {
+            id: 2,
+            img: "https://reqres.in/img/faces/7-image.jpg",
+            name: "Michael Lawson",
+            phone: 565345345,
+            email: "michael.lawson@reqres.in",
+        },
+        {
+            "id": 3,
+            img: "https://reqres.in/img/faces/8-image.jpg",
+            name: "Lindsay Ferguson",
+            phone: 56234343,
+            email: "lindsay.ferguson@reqres.in",
+        },
+        {
+            "id": 4,
+            img: "https://reqres.in/img/faces/9-image.jpg",
+            name: "Tobias Funke",
+            phone: 56435456,
+            email: "tobias.funke@reqres.in",
+        }
+    ]
+});
