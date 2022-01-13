a6b89195b7eddfd80c96f1fa45e00c9532b2db9e
BetoSandoval <alberto.sa1391@gmail.com>
2022-01-13 11:47:56 -0600

    Eventos, nombres de inputs y tipos de inputs

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,14 @@
 import { FormControl, FormLabel, Input } from "@chakra-ui/react";
 
-export const Form = () => {
+export const Form = ( {handleChange} ) => {
   return (
     <FormControl padding="4" mb={5}>
       <FormLabel htmlFor="email">Nombre Completo</FormLabel>
-      <Input id="email" type="email" bg="white.100" mb={5} />
+      <Input onChange={handleChange} type='text' name='name' id="email" type="email" bg="white.100" mb={5} />
       <FormLabel htmlFor="email">Teléfono</FormLabel>
-      <Input id="email" type="email" bg="white.100" mb={5} />
+      <Input onChange={handleChange} type='text' name='phone' id="email" type="email" bg="white.100" mb={5} />
       <FormLabel htmlFor="email">Dirección de Email</FormLabel>
-      <Input id="email" type="email" bg="white.100" mb={5} />
+      <Input onChange={handleChange} type='text' name='email' id="email" type="email" bg="white.100" mb={5} />
     </FormControl>
   );
 };
