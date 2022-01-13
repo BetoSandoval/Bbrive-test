472dc7bf813dcf78aed8835963fd7ac02f76d1ac
BetoSandoval <alberto.sa1391@gmail.com>
2022-01-13 13:31:55 -0600

    Agregando ContextProvider como componente padre

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import { UserListContainer } from "./components/UserListContainer";
 import { Routes, Route } from "react-router-dom";
+import {ContextProvider} from "./context/GlobalContext";
+import { UserListContainer } from "./components/UserListContainer";
 import { UserForm } from "./components/UserForm";
 import { Header } from "./components/Header";
 
 function App() {
   return (
-    <>
+    <ContextProvider>
       <Header />
 
       <Routes>
         <Route path="/" element={<UserListContainer />} exact />
         <Route path="/add" element={<UserForm />} />
       </Routes>
-    </>
+    </ContextProvider>
   );
 }
 
