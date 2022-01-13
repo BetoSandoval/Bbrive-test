cf230e5cc40637c16204bd4c0c6edac558bb43b5
BetoSandoval <alberto.sa1391@gmail.com>
2022-01-13 03:42:30 -0600

    Estilos UserForm y division

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,9 +1,23 @@
-
+import { Container, Button } from "@chakra-ui/react";
+import { Form } from "./Form";
+import { UploadImg } from "./UploadImg";
 
 export const UserForm = () => {
-    return (
-        <div>
-            <h1>User form</h1>
-        </div>
-    )
-}
+  return (
+    <Container
+      border="1px"
+      borderColor="gray.200"
+      borderRadius="6"
+      bg="gray.100"
+      centerContent
+    >
+      <UploadImg />
+
+      <Form/>
+      
+      <Button colorScheme="blue" mb={5}>
+        Crear nuevo Empleado
+      </Button>
+    </Container>
+  );
+};
