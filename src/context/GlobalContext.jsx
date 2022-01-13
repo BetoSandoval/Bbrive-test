c7a4ece9ea557646cecdbffdbb2aeb611d31f262
BetoSandoval <alberto.sa1391@gmail.com>
2022-01-13 11:46:14 -0600

    Cambiando tipos de datos en phone de globalContext

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -6,28 +6,28 @@ export const GlobalContext = createContext({
             id: 1,
             img: 'https://bit.ly/dan-abramov',
             name: 'Dan Abramov',
-            phone: 5652829,
+            phone: '5652829',
             email: 'danabramov@gmail.com'
         },
         {
             id: 2,
             img: "https://reqres.in/img/faces/7-image.jpg",
             name: "Michael Lawson",
-            phone: 565345345,
+            phone: '565345345',
             email: "michael.lawson@reqres.in",
         },
         {
             "id": 3,
             img: "https://reqres.in/img/faces/8-image.jpg",
             name: "Lindsay Ferguson",
-            phone: 56234343,
+            phone: '56234343',
             email: "lindsay.ferguson@reqres.in",
         },
         {
             "id": 4,
             img: "https://reqres.in/img/faces/9-image.jpg",
             name: "Tobias Funke",
-            phone: 56435456,
+            phone: '6435456',
             email: "tobias.funke@reqres.in",
         }
     ]
