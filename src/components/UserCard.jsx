6689b3af2836a131806a6d7c1777b834fdbe360b
BetoSandoval <alberto.sa1391@gmail.com>
2022-01-13 02:24:48 -0600

    Renderisando props en UserCard

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,39 +1,36 @@
 import React from "react";
 import { Box, Button, Image, Text, Stack } from "@chakra-ui/react";
 
-export const UserCard = () => {
+export const UserCard = ({ img, name, phone, email }) => {
   return (
-      <Box
-        padding="4"
-        bg="gray.100"
-        w='100%' 
-        borderWidth="1px"
-        borderRadius="lg"
-        display='flex'
-        alignItems='center'
-      >
-          <Image
-            borderRadius="full"
-            boxSize="100px"
-            src="https://bit.ly/dan-abramov"
-            alt="Dan Abramov"
-          />
+    <Box
+      padding="4"
+      bg="gray.100"
+      w="100%"
+      borderWidth="1px"
+      borderRadius="lg"
+      display="flex"
+      alignItems="center"
+      mb={10}
+    >
+      <Image borderRadius="full" boxSize="100px" src={img} alt={name} />
 
-          <Stack spacing={3}>
-            <Text fontSize="sm">Nombre: Alberto Sandoval Añorve</Text>
-            <Text fontSize="sm">Telefono: 553432323</Text>
-            <Text fontSize="sm">E-mail: maria@gmail.com</Text>
-          </Stack>
-
-          <Stack direction="row" spacing={4}>
-            <Button colorScheme="blue" variant="outline">
-              Editar
-            </Button>
-            <Button colorScheme="pink" variant="solid">
-              Eliminar
-            </Button>
-          </Stack>
-          
+      <Box ml={5} mr={5}>
+        <Stack spacing={3}>
+          <Text fontSize="sm">Nombre: {name}</Text>
+          <Text fontSize="sm">Telefono: {phone}</Text>
+          <Text fontSize="sm">E-mail: {email}</Text>
+        </Stack>
       </Box>
+
+      <Stack direction="row" spacing={4}>
+        <Button colorScheme="blue" variant="outline">
+          Editar
+        </Button>
+        <Button colorScheme="pink" variant="solid">
+          Eliminar
+        </Button>
+      </Stack>
+    </Box>
   );
 };
